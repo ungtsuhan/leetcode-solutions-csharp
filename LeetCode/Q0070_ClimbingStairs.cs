5429053e8ede681a931fdb909f4df90c548984d4
Ung Tsu Han <ungtsuhan@gmail.com>
2022-11-03 00:28:48 +0800

    Add ClimbStairs DP solution

--- LeetCode/Q0070_ClimbingStairs.cs
+++ LeetCode/Q0070_ClimbingStairs.cs
@@ -1,5 +1,22 @@
 public static class Q0070
 {
+    public static class ClimbStairs_DP
+    {
+        public static int ClimbStairs(int n)
+        {
+            int one = 1;
+            int two = 1;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                var temp = one;
+                one = one + two;
+                two = temp;   
+            }
+            return one;
+        }
+    }
+
     public static class ClimbStairs_RecursionWithMemoization
     {
         static Dictionary<int, int> memo = new Dictionary<int, int>();
@@ -48,10 +65,13 @@ public class Q0070_Tests
     [InlineData(45, 1836311903)]
     public void ClimbStairs_ReturnDistinctWays(int input, int expected)
     {
-        var result = Q0070.ClimbStairs_RecursionWithMemoization.ClimbStairs(input);
+        var result = Q0070.ClimbStairs_DP.ClimbStairs(input);
         Assert.Equal(expected, result);
 
-        // var result1 = Q0070.ClimbStairs_Recursion.ClimbStairs(input);
+        // var result1 = Q0070.ClimbStairs_RecursionWithMemoization.ClimbStairs(input);
         // Assert.Equal(expected, result1);
+
+        // var result2 = Q0070.ClimbStairs_Recursion.ClimbStairs(input);
+        // Assert.Equal(expected, result2);
     }
 }
