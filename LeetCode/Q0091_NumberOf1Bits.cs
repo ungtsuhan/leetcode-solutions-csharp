d1a163b00a3cd8a3eb6850491a0b09880465d136
Ung Tsu Han <ungtsuhan@gmail.com>
2022-11-16 20:05:13 +0800

    add unit test

--- LeetCode/Q0091_NumberOf1Bits.cs
+++ LeetCode/Q0091_NumberOf1Bits.cs
@@ -12,3 +12,16 @@ public static class Q0091
         return result;
     }
 }
+
+public class Q0091_Tests
+{
+    [Theory]
+    [InlineData(0b00000000000000000000000000001011, 3)]
+    [InlineData(0b00000000000000000000000010000000, 1)]
+    [InlineData(0b11111111111111111111111111111101, 31)]
+    public void HammingWeight_ReturnNumberOf1Bits(uint input, int expected)
+    {
+        var result = Q0091.HammingWeight(input);
+        Assert.Equal(expected, result);
+    }
+}
