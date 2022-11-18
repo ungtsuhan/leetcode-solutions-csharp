7a2fd9d4192acba0fabc7c5e9183e89252d891a7
Ung Tsu Han <ungtsuhan@gmail.com>
2022-11-18 23:43:40 +0800

    Add ValidPalindrome solution

---
+++ LeetCode/Q0125_ValidPalindrome.cs
@@ -0,0 +1,39 @@
+public static class Q0125
+{
+    public static bool IsPalindrome(string s)
+    {
+        int a = 0;
+        int b = s.Length - 1;
+        
+        while (true)
+        {
+            while (!char.IsLetterOrDigit(s[a]) && a < b)
+                a++;
+            while (!char.IsLetterOrDigit(s[b]) && a < b)
+                b--;
+
+            if (a >= b)
+                return true;
+            
+            if (char.ToUpper(s[a]) != char.ToUpper(s[b]))
+                return false;
+
+            a++;
+            b--;
+        }
+    }
+}
+
+public class Q0125_Tests
+{
+    [Theory]
+    [InlineData("A man, a plan, a canal: Panama", true)]
+    [InlineData("race a car", false)]
+    [InlineData(" ", true)]
+    [InlineData("a.", true)]
+    public void IsPalindrome_ReturnIsPalindrome(string input, bool expected)
+    {
+        var result = Q0125.IsPalindrome(input);
+        Assert.Equal(expected, result);
+    }
+}
