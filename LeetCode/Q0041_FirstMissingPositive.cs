272b1fa9420e2d4422f984c0933c4cdf517c0e19
Ung Tsu Han <ungtsuhan@gmail.com>
2022-12-01 00:02:21 +0800

    Add FindMissingPositive solution

--- LeetCode/Q0041_FirstMissingPositive.cs
+++ LeetCode/Q0041_FirstMissingPositive.cs
@@ -2,6 +2,20 @@ public static Q0041
 {
     public static int FirstMissingPositive(int[] nums)
     {
-        throw new NotImplementedException();
+        var expectedPositive = 1;
+        
+        var numList = nums.Where(x => x >= 1).Distinct().OrderBy(x => x).ToList();
+        
+        if (numList.Count >= 1 && numList[numList.Count - 1] == numList.Count)
+            return numList[numList.Count - 1] + 1;
+
+        foreach (var num in numList)
+        {
+            if (num != expectedPositive) 
+                return expectedPositive;
+            else 
+                expectedPositive++;
+        }
+        return expectedPositive;
     }
 }
