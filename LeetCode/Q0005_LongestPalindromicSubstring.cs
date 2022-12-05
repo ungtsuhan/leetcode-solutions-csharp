f750551ddd907e0da89f694a77cc6f04e3cc3ade
Ung Tsu Han <ungtsuhan@gmail.com>
2022-12-05 19:05:41 +0800

    Add LongestPalindromicSubstring solution

--- LeetCode/Q0005_LongestPalindromicSubstring.cs
+++ LeetCode/Q0005_LongestPalindromicSubstring.cs
@@ -1,7 +1,36 @@
 public static class Q0005
 {
-    public static string LongestPalindrome(string input)
+    public static string LongestPalindrome(string s)
     {
+        var longestPalindromicSubstring = string.Empty;
         
+        for (var i = 0; i < s.Length; i++)
+        {
+            // odd length
+            var l = i;
+            var r = i;
+            while (l >= 0 && r < s.Length && s[l] == s[r])
+            {
+                var currentLen = r - l + 1;
+                if (currentLen > longestPalindromicSubstring.Length)
+                    longestPalindromicSubstring = s[l..(r+1)];
+                l -= 1;
+                r += 1;
+            }
+
+            // even length
+            l = i;
+            r = i + 1;
+            while (l >= 0 && r < s.Length && s[l] == s[r])
+            {
+                var currentLen = r - l + 1; 
+                if (currentLen > longestPalindromicSubstring.Length)
+                    longestPalindromicSubstring = s[l..(r+1)];
+                l -= 1;
+                r += 1;
+            }
+        }
+         
+        return longestPalindromicSubstring;
     }
 }
