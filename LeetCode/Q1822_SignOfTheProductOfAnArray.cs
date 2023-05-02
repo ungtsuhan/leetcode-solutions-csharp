a150b80c8eeb42f185e1d41b5e7f6d17e9d491b8
ungtsuhan <ungtsuhan@gmail.com>
2023-05-02 09:32:24 +0800

    Add Q1822 solution

---
+++ LeetCode/Q1822_SignOfTheProductOfAnArray.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode
+{
+    public class Q1822
+    {
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+
+        public static int ArraySign(int[] nums)
+        {
+            int sign = 1;
+        
+            foreach (var num in nums)
+            {
+                if (num == 0) 
+                    return 0;
+
+                if (num < 0)
+                    sign = -sign;
+            }
+
+            return sign;
+        }
+    }
+}
