22c10645e174c317977f829a79266b8942568bbb
ungtsuhan <ungtsuhan@gmail.com>
2023-05-02 12:09:01 +0800

    Add Q0258 solution

---
+++ LeetCode/Q0258_AddDigits.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode
+{
+    public class Q0258
+    {
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+
+        public static int AddDigits(int num)
+        {
+            while (num >= 10)
+                num = num / 10 + num % 10;
+        
+            return num; 
+        }
+    }
+}
