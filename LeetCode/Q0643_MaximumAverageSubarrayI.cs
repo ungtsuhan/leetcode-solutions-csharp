f38481bc7ee7938af29d6e5baf34477d1cc51579
ungtsuhan <ungtsuhan@gmail.com>
2023-05-06 00:19:12 +0800

    Add Q0643 solution

---
+++ LeetCode/Q0643_MaximumAverageSubarrayI.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode
+{
+    public class Q0643
+    {
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+
+        public static double FindMaxAverage(int[] nums, int k)
+        {
+            var currentSum = 0;
+
+            for (var i = 0; i < k; i++)
+            {
+                currentSum += nums[i];
+            }
+
+            var maxSum = currentSum;
+
+            for (var i = k; i < nums.Length; i++)
+            {
+                currentSum += nums[i] - nums[i - k];
+                maxSum = Math.Max(currentSum, maxSum);
+            }
+
+            return maxSum / (double)k;
+        }
+    }
+}
