853173fbbf8fef16c06ec6b4e004c7e329454e81
ungtsuhan <ungtsuhan@gmail.com>
2023-05-07 21:16:42 +0800

    Add Q1876 solution

---
+++ LeetCode/Q1876_SubstringsOfSizeThreeWithDistinctCharacters.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode
+{
+    public class Q1876
+    {
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+
+        public static int CountGoodSubstrings(string s)
+        {
+            var count = 0;
+
+            for (var i = 1; i < s.Length - 1; i++)
+            {
+                if (s[i] != s[i - 1] && s[i] != s[i + 1] && s[i - 1] != s[i + 1])
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
