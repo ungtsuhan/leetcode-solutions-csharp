50492ff3671039417a6fbc88d5310ee7911d283a
ungtsuhan <ungtsuhan@gmail.com>
2023-05-07 22:08:38 +0800

    Add Q1498 solution

---
+++ LeetCode/Q1498_NumberOfSubsequencesThatSatisfyTheGivenSumCondition.cs
@@ -0,0 +1,33 @@
+﻿using System.Numerics;
+
+namespace LeetCode
+{
+    public class Q1498
+    {
+        public static int NumSubseq(int[] nums, int target)
+        {
+            Array.Sort(nums);
+
+            const int MODULUS = 1_000_000_007;
+
+            int left = 0;
+            int right = nums.Length - 1;
+            BigInteger totalCount = 0;
+
+            while (left <= right)
+            {
+                if (nums[left] + nums[right] > target)
+                {
+                    right--;
+                }
+                else
+                {
+                    totalCount += BigInteger.ModPow(2, right - left, MODULUS) % MODULUS;
+                    left++;
+                }
+            }
+
+            return (int)(totalCount);
+        }
+    }
+}
