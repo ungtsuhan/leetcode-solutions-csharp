6a4c5d0cfd8dc7661933862db2a3009eab210e69
ungtsuhan <ungtsuhan@gmail.com>
2023-05-08 14:38:15 +0800

    Add Q1572 solution

---
+++ LeetCode/Q1572_MatrixDiagonalSum.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode
+{
+    public class Q1572
+    {
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+    
+        public static int DiagonalSum(int[][] mat)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < mat.Length; i++)
+                sum += mat[i][i] + mat[i][mat.Length-1-i];
+
+            if (mat.Length % 2 != 0)
+                sum -= mat[mat.Length/2][mat.Length/2];
+
+            return sum;
+        }
+    }
+}
