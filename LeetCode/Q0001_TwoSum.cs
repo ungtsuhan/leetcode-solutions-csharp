c7492382fad7c1a785e60321f097d42efc9f8950
ungtsuhan <ungtsuhan@gmail.com>
2023-05-10 22:37:41 +0800

    Add Q0001 solution

---
+++ LeetCode/Q0001_TwoSum.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode
+{
+    public class Q0001
+    {
+        // Time Complexity: O(n)
+        // Space Complexity: O(n)
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var dict = new Dictionary<int, int>();
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var complement = target - nums[i];
+
+                if (!dict.ContainsKey(nums[i]))
+                    dict.Add(complement, i);
+                else
+                    return new int[] { dict[nums[i]], i };
+            }
+
+            throw new ArgumentException("No two numbers in the input array add up to the target value.");
+        }
+    }
+}
