85383a2ef16a92ebb56de3810788aa38b5768bc9
ungtsuhan <ungtsuhan@gmail.com>
2023-05-10 22:01:06 +0800

    Add Q0217 solution

---
+++ LeetCode/Q0217_ContainsDuplicate.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode
+{
+    public class Q0217
+    {
+        // Time Complexity: O(n)
+        // Space Complexity: O(n)
+
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            var set = new HashSet<int>();
+
+            foreach (var num in nums)
+            {
+                if (set.Contains(num))
+                    return true;
+                else
+                    set.Add(num);
+            }
+
+            return false;
+        }
+    }
+}
