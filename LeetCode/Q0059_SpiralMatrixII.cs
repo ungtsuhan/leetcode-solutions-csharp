fa0bbe1c5fefc7f1301b108f3e3cf3e52b094535
ungtsuhan <ungtsuhan@gmail.com>
2023-05-10 20:28:20 +0800

    Add Q0059 solution

---
+++ LeetCode/Q0059_SpiralMatrixII.cs
@@ -0,0 +1,52 @@
+﻿namespace LeetCode
+{
+    public class Q0059
+    {
+        // Time Complexity: O(n^2)
+        // Space Complexity: O(n^2)
+
+        public static int[][] GenerateMatrix(int n)
+        {
+            int rowBegin = 0;
+            int rowEnd = n - 1;
+            int colBegin = 0;
+            int colEnd = n - 1;
+            int count = 1;
+
+            int[][] matrix = new int[n][];
+            for (int i = 0; i < n; i++)
+                matrix[i] = new int[n];
+
+            while (rowBegin <= rowEnd && colBegin <= colEnd)
+            {
+                // Traverse Right
+                for (var i = colBegin; i <= colEnd; i++)
+                    matrix[rowBegin][i] = count++;
+                rowBegin++;
+
+                // Traverse Down
+                for (var i = rowBegin; i <= rowEnd; i++)
+                    matrix[i][colEnd] = count++;
+                colEnd--;
+
+                // Traverse Left
+                if (rowBegin <= rowEnd)
+                {
+                    for (var i = colEnd; i >= colBegin; i--)
+                        matrix[rowEnd][i] = count++;
+                }
+                rowEnd--;
+
+                // Traverse Up
+                if (colBegin <= colEnd)
+                {
+                    for (var i = rowEnd; i >= rowBegin; i--)
+                        matrix[i][colBegin] = count++;
+                }
+                colBegin++;
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ LeetCodeTests/Q0059_SpiralMatrixIITests.cs
@@ -0,0 +1,26 @@
+using LeetCode;
+
+namespace LeetCodeTests
+{
+    public class Q0059Tests
+    {
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[] { 1, new[] { new[] { 1 } } };
+            yield return new object[] { 3, new[] { new[] { 1, 2, 3 }, new[] { 8, 9, 4 }, new[] { 7, 6, 5 } } };
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void _ValidInput_ReturnCorrectMatrix(int n, int[][] expectedMatrix)
+        {
+            // Arrange
+
+            // Act
+            var actualMatrix = Q0059.GenerateMatrix(n);
+
+            // Assert
+            Assert.Equal(expectedMatrix, actualMatrix);
+        }
+    }
+}
