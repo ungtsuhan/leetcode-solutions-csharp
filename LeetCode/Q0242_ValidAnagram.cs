d73d5e303f44fb311a8b57dd2c3554f719d5c557
ungtsuhan <ungtsuhan@gmail.com>
2023-05-10 23:54:25 +0800

    Add Q0242 solution

---
+++ LeetCode/Q0242_ValidAnagram.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode
+{
+    public class Q0242
+    {
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+
+        public static bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+                return false;
+
+            var charCounts = new int[26];
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                charCounts[s[i] - 'a']++;
+                charCounts[t[i] - 'a']--;
+            }
+
+            foreach (var charCount in charCounts)
+            {
+                if (charCount != 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCodeTests/Q0242_ValidAnagramTests.cs
@@ -0,0 +1,22 @@
+using LeetCode;
+
+namespace LeetCodeTests
+{
+    public class Q0242Tests
+    {
+        [Theory]
+        [InlineData("anagram", "nagaram", true)]
+        [InlineData("rat", "car", false)]
+        [InlineData("abc", "b", false)]
+        public void IsAnagram_ValidInput_ReturnsCorrectResult(string s, string t, bool expectedResult)
+        {
+            // Arrange
+
+            // Act
+            var actualResult = Q0242.IsAnagram(s, t);
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
