c8b16e18cb3b5b64c34eb0b38167e22ea76cb628
ungtsuhan <ungtsuhan@gmail.com>
2023-05-11 23:44:26 +0800

    Add Q2652 solution

---
+++ LeetCode/Q2652_SumMultiples.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode
+{
+    public class Q2652
+    {
+        public static int SumOfMultiples(int n)
+        {
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0)
+                    sum += i;
+            }
+            return sum;
+        }
+    }
+}
---
+++ LeetCodeTests/Q2652_SumMultiplesTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode;
+
+namespace LeetCodeTests
+{
+    public class Q2652Tests
+    {
+        [Theory]
+        [InlineData(7, 21)]
+        [InlineData(10, 40)]
+        [InlineData(9, 30)]
+        public void SumOfMultiples_ValidInput_ReturnsCorrectSum(int n, int expectedSum)
+        {
+            // Arrange
+
+            // Act
+            var actualSum = Q2652.SumOfMultiples(n);
+
+            // Assert
+            Assert.Equal(expectedSum, actualSum);
+        }
+    }
+}
