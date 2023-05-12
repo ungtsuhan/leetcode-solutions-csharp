5af19bf5ac9f84a3ee6ee1b2d5f9eb1651e78e0c
ungtsuhan <ungtsuhan@gmail.com>
2023-05-12 23:26:40 +0800

    Add Q0347 solution

---
+++ LeetCode/Q0347_TopKFrequentElements.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode
+{
+    public class Q0347
+    {
+        // Time Complexity: O(n log k)
+        // Space Complexity: O(n)
+
+        public static int[] TopKFrequent(int[] nums, int k)
+        {
+            var frequencyDict = new Dictionary<int, int>();
+            foreach (var num in nums) // O(n)
+            {
+                if (frequencyDict.TryGetValue(num, out int count))
+                    frequencyDict[num] = count + 1;
+                else
+                    frequencyDict.Add(num, 1);
+            }
+
+            var heap = new PriorityQueue<int, int>();
+            foreach (var element in frequencyDict) // O(n log k)
+            {
+                heap.Enqueue(element.Key, element.Value);
+                if (heap.Count > k)
+                    heap.Dequeue();
+            }
+
+            var result = new int[k];
+            for (var j = 0; j < k; j++) // O(k log k)
+                result[j] = heap.Dequeue();
+
+            return result;
+        }
+    }
+}
