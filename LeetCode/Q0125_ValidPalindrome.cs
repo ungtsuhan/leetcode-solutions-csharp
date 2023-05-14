c9d5a7996b16e1814ae63dbffa871e68ecd2f3a7
ungtsuhan <ungtsuhan@gmail.com>
2023-05-14 11:07:50 +0800

    Add Q0125 solution

---
+++ LeetCode/Q0125_ValidPalindrome.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode
+{
+    public class Q0125
+    {
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+
+        public static bool IsPalindrome(string s)
+        {
+            int l = 0;
+            int r = s.Length - 1;
+            
+            while (l < r)
+            {
+                if (!char.IsLetterOrDigit(s[l]))
+                {
+                    l++;
+                    continue;
+                }
+
+                if (!char.IsLetterOrDigit(s[r]))
+                {
+                    r--;
+                    continue;
+                }
+
+                if (char.ToLower(s[l]) != char.ToLower(s[r]))
+                    return false;
+
+                l++;
+                r--;
+            }
+
+            return true;
+        }
+    }
+}
