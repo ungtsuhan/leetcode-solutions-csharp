a6d6b0aae3a0ace7389f7078febefce19b3d1f46
ungtsuhan <ungtsuhan@gmail.com>
2023-05-15 23:31:23 +0800

    Add ListNode Model and Helper

---
+++ LeetCode.Common/Helpers/ListNodeHelper.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.Models.ListNode;
+
+namespace LeetCode.Common.Helpers
+{
+    public class ListNodeHelper
+    {
+        public static ListNode? BuildList(int[] values)
+        {
+            if (values == null || values.Length == 0) return null;
+            var head = new ListNode(values[0]);
+            var current = head;
+            for (var i = 1; i < values.Length; i++)
+            {
+                current.next = new ListNode(values[i]);
+                current = current.next;
+            }
+            return head;
+        }
+
+        public static int[] ListToArray(ListNode head)
+        {
+            var result = new List<int>();
+            var current = head;
+            while (current != null)
+            {
+                result.Add(current.val);
+                current = current.next;
+            }
+            return result.ToArray();
+        }
+    }
+}
---
+++ LeetCode.Common/Models/ListNode.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Models.ListNode
+{
+    /// <summary>
+    /// Definition for singly-linked list
+    /// </summary>
+    public class ListNode
+    {
+        public int val;
+        public ListNode? next;
+
+        public ListNode(int val = 0, ListNode? next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
