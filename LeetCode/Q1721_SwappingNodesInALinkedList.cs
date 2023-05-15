094c3f95429d122a48d2a8a2833f115ad5fcd676
ungtsuhan <ungtsuhan@gmail.com>
2023-05-15 23:31:54 +0800

    Add Q1721 solution

---
+++ LeetCode/Q1721_SwappingNodesInALinkedList.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.Models.ListNode;
+
+namespace LeetCode
+{
+    public class Q1721
+    {
+        public static ListNode SwapNodes(ListNode head, int k)
+        {
+            var curr = head;
+
+            for (var i = 1; i < k; i++)
+                curr = curr.next;
+
+            var left = curr;
+            var right = head;
+
+            while (curr.next != null)
+            {
+                curr = curr.next;
+                right = right.next;
+            }
+
+            var temp = left.val;
+            left.val = right.val;
+            right.val = temp;
+
+            return head;
+        }
+    }
+}
---
+++ LeetCodeTests/Q1721_SwappingNodesInALinkedListTests.cs
@@ -0,0 +1,24 @@
+using LeetCode;
+using LeetCode.Common.Helpers;
+
+namespace LeetCodeTests
+{
+    public class Q1721Tests
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, 2, new[] { 1, 4, 3, 2, 5 })]
+        [InlineData(new[] { 7, 9, 6, 6, 7, 8, 3, 0, 9, 5 }, 5, new[] { 7, 9, 6, 6, 8, 7, 3, 0, 9, 5 })]
+        public void SwapNodes_ValidInput_ReturnsCorrectLinkedListAfterSwapNodes(int[] inputValues, int k, int[] expectedValues)
+        {
+            // Arrange
+            var list = ListNodeHelper.BuildList(inputValues);
+
+            // Act
+            var actualLinkedList = Q1721.SwapNodes(list, k);
+            var actualValues = ListNodeHelper.ListToArray(actualLinkedList);
+
+            // Assert
+            Assert.Equal(expectedValues, actualValues);
+        }
+    }
+}
