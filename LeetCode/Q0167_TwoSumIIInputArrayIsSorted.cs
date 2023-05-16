3c549c28686ae1a9f59a1fb127119d9af3b2f858
ungtsuhan <ungtsuhan@gmail.com>
2023-05-16 22:39:45 +0800

    Add Q0167 solution

---
+++ LeetCode/Q0167_TwoSumIIInputArrayIsSorted.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode
+{
+    public class Q0167
+    {
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            int left = 0;
+            int right = numbers.Length - 1;
+
+            while (left < right)
+            {
+                int sum = numbers[left] + numbers[right];
+
+                if (sum > target)
+                    right--;
+                else if (sum < target)
+                    left++;
+                else
+                    return new[] { left + 1, right + 1 };
+            }
+
+            throw new ArgumentException("No valid pairs found for the given target.");
+        }
+    }
+}
