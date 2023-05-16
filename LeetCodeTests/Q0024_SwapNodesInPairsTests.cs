534c922212b19311fa4205105520996c1770a57f
ungtsuhan <ungtsuhan@gmail.com>
2023-05-16 14:30:09 +0800

    Add Q0024 solution

--- LeetCodeTests/Q0024_SwapNodesInPairsTests.cs
+++ LeetCodeTests/Q0024_SwapNodesInPairsTests.cs
@@ -9,7 +9,7 @@ namespace LeetCodeTests
         [InlineData(new int[] { 1,2,3,4 }, new int[] { 2, 1, 4,3 })]
         [InlineData(new int[] { 1 }, new int[] { 1 })]
         [InlineData(new int[] { }, new int[] { })]
-        public void IsPalindrome_ValidInput_ReturnsCorrectResult(int[] head, int[] expectedValues)
+        public void SwapPairs_ValidInput_ReturnsCorrectListNodeAfterSwap(int[] head, int[] expectedValues)
         {
             // Arrange
             var list = ListNodeHelper.CreateLinkedList(head);
