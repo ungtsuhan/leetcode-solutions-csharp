7617ae9197b5617a390a71370ce4163142be5efe
ungtsuhan <ungtsuhan@gmail.com>
2023-05-16 21:11:03 +0800

    Add Q0011 solution

---
+++ LeetCode/Q0011_ContainerWithMostWater.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode
+{
+    public class Q0011
+    {
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+
+        public static int MaxArea(int[] height)
+        {
+            int maxArea = 0;
+            int left = 0;
+            int right = height.Length - 1;
+
+            while (left < right)
+            {
+                int currentArea = Math.Min(height[left], height[right]) * (right - left);
+                maxArea = Math.Max(currentArea, maxArea);
+
+                if (height[left] < height[right])
+                    left++;
+                else
+                    right--;
+            }
+
+            return maxArea;
+        }
+    }
+}
---
+++ LeetCodeTests/Q0011_ContainerWithMostWaterTests.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode;
+
+namespace LeetCodeTests
+{
+    public class Q0011Tests
+    {
+        [Theory]
+        [InlineData(new[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }, 49)]
+        [InlineData(new[] { 1, 1 }, 1)]
+        public void Test(int[] height, int expectedMaxArea)
+        {
+            // Arrange
+
+            // Act
+            var actualMaxArea = Q0011.MaxArea(height);
+
+            // Assert
+            Assert.Equal(expectedMaxArea, actualMaxArea);
+        }
+    }
+}
