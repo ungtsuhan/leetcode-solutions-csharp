b05e1fef2c2f9b0d283efdfd09b72f49c874462f
ungtsuhan <ungtsuhan@gmail.com>
2023-05-17 22:41:26 +0800

    Add Q2130 solution

---
+++ LeetCodeTests/Q2130_MaximumTwinSumOfALinkedListTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode;
+using LeetCode.Common.Helpers;
+
+namespace LeetCodeTests
+{
+    public class Q2130Tests
+    {
+        [Theory]
+        [InlineData(new[] { 5, 4, 2, 1 }, 6)]
+        [InlineData(new[] { 4, 2, 2, 3 }, 7)]
+        [InlineData(new[] { 1, 100000 }, 100001)]
+        public void PairSum_ValidInput_ReturnsCorrectMaxTwinSum(int[] listNodeValues, int expectedMaxTwinSum) 
+        {
+            // Arrange
+            var listNode = ListNodeHelper.CreateLinkedList(listNodeValues);
+
+            // Act
+            var actualMaxTwinSum = Q2130.PairSum(listNode);
+
+            // Assert
+            Assert.Equal(actualMaxTwinSum, expectedMaxTwinSum);
+        }
+    }
+}
