a1741884a1ff031069f22c7ef52a68b974ceac54
ungtsuhan <ungtsuhan@gmail.com>
2023-05-18 22:48:58 +0800

    Add Q1557 solution

---
+++ LeetCode/Q1557_MinimumNumberOfVerticesToReachAllNodes.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode
+{
+    public class Q1557
+    {
+        // Time Complexity: O(e + v)
+        // Space Complexity: O(e + v)
+
+        public static IList<int> FindSmallestSetOfVertices(int n, IList<IList<int>> edges)
+        {
+            // Intuition: Find nodes without incoming edges
+
+            var incoming = new HashSet<int>();
+            foreach (var edge in edges)
+                incoming.Add(edge[1]);
+
+            var result = new List<int>();
+            for (var i = 0; i < n; i++)
+                if (!incoming.Contains(i))
+                    result.Add(i);
+
+            return result;
+        }
+    }
+}
