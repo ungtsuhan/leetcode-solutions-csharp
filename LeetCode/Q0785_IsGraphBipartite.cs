27a088251b9e09c6eae4cc2f5c62473f5a673451
ungtsuhan <ungtsuhan@gmail.com>
2023-05-19 23:39:59 +0800

    Add Q0785 solution

---
+++ LeetCode/Q0785_IsGraphBipartite.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCode
+{
+    public class Q0785
+    {
+        // Time Complexity: O(v + e)
+        // Space Complexity: O(v)
+
+        public static bool IsBipartite(int[][] graph)
+        {
+            var array = new int[graph.Length];
+            var queue = new Queue<int>();
+
+            for (int i = 0; i < graph.Length; i++)
+            {
+                if (array[i] != 0)
+                    continue;
+
+                queue.Enqueue(i);
+                array[i] = 1; // odd = 1, even = -1
+
+                while (queue.Count > 0)
+                {
+                    int num = queue.Dequeue();
+
+                    foreach (int neighbour in graph[num])
+                    {
+                        if (array[neighbour] == 0)
+                        {
+                            queue.Enqueue(neighbour);
+                            array[neighbour] = -array[num];
+                        }
+                        else if (array[neighbour] == array[num])
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
