35d375a5eaff4bf838a9f8bd66814c94e4434149
ungtsuhan <ungtsuhan@gmail.com>
2023-05-20 15:42:19 +0800

    Add Q0020 solution

---
+++ LeetCode/Q0020_ValidParentheses.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode
+{
+    public class Q0020
+    {
+        // Time Complexity: O(n)
+        // Space Complexity: O(n)
+
+        public static bool IsValid(string s)
+        {
+            var dict = new Dictionary<char, char>()
+            {
+                {'(', ')'},
+                {'{', '}'},
+                {'[', ']'},
+            };
+
+            var stack = new Stack<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (dict.TryGetValue(s[i], out char value))
+                    stack.Push(value);
+
+                else if (stack.Count == 0 || stack.Pop() != s[i])
+                    return false;
+            }
+            return stack.Count == 0;
+        }
+    }
+}
