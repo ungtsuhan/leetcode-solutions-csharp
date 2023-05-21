9ffc6d853112dad72ec5b2d3ee3c73fb11563c88
ungtsuhan <ungtsuhan@gmail.com>
2023-05-21 09:41:34 +0800

    Add Q0206 solution

---
+++ LeetCode/Q0206_ReverseLinkedList.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Models.ListNode;
+
+namespace LeetCode
+{
+    public class Q0206
+    {
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+
+        public static ListNode? ReverseList(ListNode head)
+        {
+            ListNode? prev = null;
+            ListNode? curr = head;
+
+            while (curr != null)
+            {
+                ListNode? next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+
+            return prev;
+        }
+    }
+}
