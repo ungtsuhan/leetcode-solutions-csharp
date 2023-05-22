8a4fa14f4d5d5085f1f30a98be6854de4bd70685
ungtsuhan <ungtsuhan@gmail.com>
2023-05-22 22:04:47 +0800

    Add Q0021 solution

---
+++ LeetCode/Q0021_MergeTwoSortedLists.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode.Models.ListNode;
+
+namespace LeetCode
+{
+    public class Q0021
+    {
+        // Time Complexity: O(n + m)
+        // Space Complexity: O(1)
+
+        public static ListNode? MergeTwoLists(ListNode? list1, ListNode? list2)
+        {
+            var head = new ListNode();
+            var tail = head;
+
+            while (list1 != null && list2 != null)
+            {
+                if (list1.val < list2.val)
+                {
+                    tail.next = list1;
+                    list1 = list1.next;
+                }
+                else
+                {
+                    tail.next = list2;
+                    list2 = list2.next;
+                }
+                tail = tail.next;
+            }
+
+            if (list1 != null) tail.next = list1;
+            if (list2 != null) tail.next = list2;
+
+            return head.next;
+        }
+    }
+}
