4af249621a48f94f99c5c40317c1fd96c0813e22
ungtsuhan <ungtsuhan@gmail.com>
2023-05-24 23:52:18 +0800

    Add Q2542 solution

---
+++ LeetCode/Q2542_MaximumSubsequenceScore.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode
+{
+    public class Q2542
+    {
+        // Time Complexity: O(nlog(n))
+        // Space Complexity: O(k)
+
+        public static long MaxScore(int[] nums1, int[] nums2, int k)
+        {
+            // Sort nums1 based on nums2 in descending order
+            Array.Sort(nums2, nums1, Comparer<int>.Create((x, y) => y.CompareTo(x))); // O(nlog(n))
+
+            PriorityQueue<int, int> heap = new();
+            int sum = 0;
+            int result = 0;
+
+            for (int i = 0; i < nums1.Length; i++) // TC: O(nlog(k))
+            {
+                heap.Enqueue(nums1[i], nums1[i]); // TC: O(log(k))
+                sum += nums1[i];
+
+                if (heap.Count > k)
+                    sum -= heap.Dequeue(); // TC: O(log(k))
+
+                if (heap.Count == k)
+                    result = Math.Max(result, sum * nums2[i]);
+            }
+
+            return result;
+        }
+    }
+}
