103b03cf47034fd2b581971e77c117badfc687f5
ungtsuhan <ungtsuhan@gmail.com>
2023-05-25 23:31:14 +0800

    Add Q0014 solution

---
+++ LeetCode.Tests/Q0014_LongestCommonPrefixTests.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Tests
+{
+    public class Q0014Tests
+    {
+        [Theory]
+        [InlineData(new[] { "flower", "flow", "flight" }, "fl")]
+        [InlineData(new[] { "dog", "racecar", "car" }, "")]
+        public void LongestCommonPrefix_ValidInput_ReturnsCorrectCommonPrefix(string[] strs, string expectedCommonPrefix)
+        {
+            // Arrange
+
+            // Act
+            var actualCommonPrefix = Q0014.LongestCommonPrefix(strs);
+
+            // Assert
+            Assert.Equal(expectedCommonPrefix, actualCommonPrefix);
+        }
+    }
+}
---
+++ LeetCode/Q0014_LongestCommonPrefix.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode
+{
+    public class Q0014
+    {
+        // Time Complexity: O(n * m)
+        // Space Complexity: O(m)
+
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0) return string.Empty;
+            if (strs.Length == 1) return strs[0];
+
+            int index = 0;
+            string commonPrefix = string.Empty;
+
+            while (true)
+            {
+                char charToCompare = default;
+                for (int j = 0; j < strs.Length; j++)
+                {
+                    if (index >= strs[j].Length)
+                        return commonPrefix;
+
+                    if (j == 0)
+                        charToCompare = strs[j][index];
+
+                    else if (strs[j][index] != charToCompare)
+                        return commonPrefix;
+                }
+                commonPrefix += charToCompare;
+                index++;
+            }
+        }
+    }
+}
