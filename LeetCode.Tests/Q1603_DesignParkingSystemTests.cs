522a262166d2853ad0482d26f457e0fc3f1c5612
ungtsuhan <ungtsuhan@gmail.com>
2023-05-29 13:57:52 +0800

    Add Q1603 solution

---
+++ LeetCode.Tests/Q1603_DesignParkingSystemTests.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Tests
+{
+    public class ParkingSystemTests
+    {
+        [Theory]
+        [InlineData(1, 1, 0, new[] { 1, 2, 3, 1 }, new[] { true, true, false, false })]
+        public void AddCar_ValidInput_ReturnsCorrectParkingLotAvailability(int big, int medium, int small, int[] carsToBeAdded, bool[] expectedResult)
+        {
+            // Arrange
+             var parkingSystem = new ParkingSystem(big, medium, small);
+
+            // Act
+            var actualResult = new List<bool>();
+            foreach (var car in carsToBeAdded)
+                actualResult.Add(parkingSystem.AddCar(car));
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult.ToArray());
+        }
+    }
+}
---
+++ LeetCode/Q1603_DesignParkingSystem.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode
+{
+    public class ParkingSystem
+    {
+        // Time Complexity: O(1)
+        // Space Complexity: O(1)
+
+        private readonly Dictionary<int, int> availableSlot;
+        public ParkingSystem(int big, int medium, int small)
+        {
+            availableSlot = new Dictionary<int, int>
+            {
+                { 1, big },
+                { 2, medium },
+                { 3, small }
+            };
+        }
+    
+        public bool AddCar(int carType)
+        {
+            if (!availableSlot.ContainsKey(carType) || availableSlot[carType] == 0)
+                return false;
+
+            availableSlot[carType]--;
+            return true;
+        }
+    }
+}
