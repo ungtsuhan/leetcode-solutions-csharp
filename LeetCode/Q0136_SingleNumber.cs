4891118a35de0b3f2eba9f8e5a538ea4047bc0d6
ungtsuhan <ungtsuhan@gmail.com>
2023-06-01 23:12:54 +0800

    Add Q0136 solution

---
+++ LeetCode/Q0136_SingleNumber.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode
+{
+    public class Q0136
+    {
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+
+        public static int SingleNumber(int[] nums)
+        {
+            var xor = 0;
+        
+            foreach (var num in nums)
+                xor ^= num;
+
+            return xor;
+        }
+    }
+}
