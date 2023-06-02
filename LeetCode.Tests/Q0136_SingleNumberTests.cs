1525964f3f4a47ec93dbcc130b4154125a2aa690
ungtsuhan <ungtsuhan@gmail.com>
2023-06-02 17:10:38 +0800

    Add Q0136 solution

---
+++ LeetCode.Tests/Q0136_SingleNumberTests.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Tests
+{
+    public class Q0136Tests
+    {
+        [Theory]
+        [InlineData(new[] { 2, 2, 1 }, 1)]
+        [InlineData(new[] { 4, 1, 2, 1, 2 }, 4)]
+        [InlineData(new[] { 1 }, 1)]
+        public void SingleNumber_ValidInput_ReturnsCorrectResult(int[] nums, int expectedResult)
+        {
+            // Arrange
+
+            // Act
+            var actualResult = Q0136.SingleNumber(nums);
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
