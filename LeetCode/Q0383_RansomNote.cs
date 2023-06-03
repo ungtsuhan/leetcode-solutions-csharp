e907e9adc5288fb31272df9b666b5d3b4a9c8605
ungtsuhan <ungtsuhan@gmail.com>
2023-06-03 17:04:17 +0800

    Add Q0383 solution

---
+++ LeetCode/Q0383_RansomNote.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode
+{
+    public class Q0383
+    {
+        // Time Complexity: O(n + m)
+        // Space Complexity: O(1)
+
+        public static bool CanConstruct(string ransomNote, string magazine)
+        {
+            var queueLength = ransomNote.Length;
+            var charCount = new Dictionary<char, int>();
+
+            foreach (var ransomNoteChar in ransomNote)
+            {
+                if (charCount.ContainsKey(ransomNoteChar))
+                    charCount[ransomNoteChar]++;
+                else
+                    charCount[ransomNoteChar] = 1;
+            }
+
+            foreach (var magazineChar in magazine)
+            {
+                if (charCount.ContainsKey(magazineChar) && charCount[magazineChar] > 0)
+                {
+                    queueLength--;
+                    charCount[magazineChar]--;
+
+                    if (queueLength == 0)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
