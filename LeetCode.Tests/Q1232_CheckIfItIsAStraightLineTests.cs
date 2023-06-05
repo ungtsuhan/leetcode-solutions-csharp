830d6ecde8d6ddf76f49c7dc05ae1779a7fa9168
ungtsuhan <ungtsuhan@gmail.com>
2023-06-05 23:15:29 +0800

    Add Q1232 solution

---
+++ LeetCode.Tests/Q1232_CheckIfItIsAStraightLineTests.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Tests
+{
+    public class Q1232Tests
+    {
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[] { new int[][] { new int[] { 1, 2 }, new int[] { 2, 3 }, new int[] { 3, 4 }, new int[] { 4, 5 }, new int[] { 5, 6 }, new int[] { 6, 7 } }, true };
+            yield return new object[] { new int[][] { new int[] { 1, 1 }, new int[] { 2, 2 }, new int[] { 3, 4 }, new int[] { 4, 5 }, new int[] { 5, 6 }, new int[] { 7, 7 } }, false };
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void CheckStraightLine_ValidInput_ReturnsCorrectResult(int[][] coordinates, bool expectedResult)
+        {
+            // Arrange
+
+            // Act
+            var actualResult = Q1232.CheckStraightLine(coordinates);
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
---
+++ LeetCode/Q1232_CheckIfItIsAStraightLine.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode
+{
+    public class Q1232
+    {
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+
+        public static bool CheckStraightLine(int[][] coordinates)
+        {
+            var deltaX = GetXDiff(coordinates[1][0], coordinates[0][0]);
+            var deltaY = GetYDiff(coordinates[1][1], coordinates[0][1]);
+
+            for (var i = 2; i < coordinates.Length; i++)
+            {
+                if (deltaY * GetXDiff(coordinates[i][0], coordinates[0][0]) != deltaX * GetYDiff(coordinates[i][1], coordinates[0][1]))
+                    return false;
+            }
+            return true;
+        }
+
+        private static int GetXDiff(int x1, int x0) => x1 - x0;
+        private static int GetYDiff(int y1, int y0) => y1 - y0;
+    }
+}
