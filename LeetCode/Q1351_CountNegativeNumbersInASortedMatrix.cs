6bbdd2be0e1d0471cb1de029e73c02985c89e6df
ungtsuhan <ungtsuhan@gmail.com>
2023-06-08 19:11:12 +0800

    Add Q1351 solution

---
+++ LeetCode/Q1351_CountNegativeNumbersInASortedMatrix.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode
+{
+    public class Q1351
+    {
+        // Time Complexity: O(m * n)
+        // Space Complexity: O(1)
+
+        public static int CountNegatives(int[][] grid)
+        {
+            var negativeItemCount = 0;
+            foreach (var row in grid)
+            {
+                foreach (var item in row)
+                {
+                    if (item < 0)
+                        negativeItemCount++;
+                }
+            }
+            return negativeItemCount;
+        }
+    }
+}
