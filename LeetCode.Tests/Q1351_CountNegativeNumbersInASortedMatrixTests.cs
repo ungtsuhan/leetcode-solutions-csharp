11a300968ccb7dbf0f91ec5e71a529da56407648
ungtsuhan <ungtsuhan@gmail.com>
2023-06-08 19:16:42 +0800

    Add Q1351 solution

---
+++ LeetCode.Tests/Q1351_CountNegativeNumbersInASortedMatrixTests.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode.Tests
+{
+    public class Q1351Tests
+    {
+        public static IEnumerable<object[]> CountNegativesTestData()
+        {
+            yield return new object[]
+            {
+                new int[][] {
+                    new int[] { 4, 3, 2, -1 },
+                    new int[] { 3, 2, 1, -1 },
+                    new int[] { 1, 1, -1, -2 },
+                    new int[] { -1, -1, -2, -3 }
+                },
+                8
+            };
+
+            yield return new object[]
+            {
+                new int[][] {
+                    new int[] { 3, 2 },
+                    new int[] { 1, 0 }
+                },
+                0
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(CountNegativesTestData))]
+        public void CountNegatives_ValidInput_ReturnsCorrectNegativeCount(int[][] grid, int expectedCount)
+        {
+            // Arrange
+
+            // Act
+            var actualCount = Q1351.CountNegatives(grid);
+        
+            // Assert
+            Assert.Equal(expectedCount, actualCount);
+        }
+    }
+}
