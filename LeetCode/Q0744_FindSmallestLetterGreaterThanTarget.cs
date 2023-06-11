b24f58915eac47757afb9d61c38d1bb56ecb25e2
ungtsuhan <ungtsuhan@gmail.com>
2023-06-11 11:29:44 +0800

    Add Q0744 solution

---
+++ LeetCode/Q0744_FindSmallestLetterGreaterThanTarget.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode
+{
+    public class Q0744
+    {
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+
+        public static char NextGreatestLetter(char[] letters, char target)
+        {
+            var resultExists = letters[^1] > target;
+
+            if (!resultExists)
+                return letters[0];
+
+            foreach (var letter in letters)
+            {
+                if (letter > target)
+                    return letter;
+            }
+
+            throw new ArgumentException("No result found for the given input.");
+        }
+    }
+}
