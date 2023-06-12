bdb034b7d2ddaa22438f19648b9a28200b2f5e41
ungtsuhan <ungtsuhan@gmail.com>
2023-06-12 22:47:21 +0800

    Add Q0228 solution

---
+++ LeetCode/Q0228_SummaryRanges.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode
+{
+    public class Q0228
+    {
+        // Time Complexity: O(n)
+        // Space Complexity: O(n)
+
+        public static IList<string> SummaryRanges(int[] nums)
+        {
+            var result = new List<string>();
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                int start = nums[i];
+
+                while(i + 1 < nums.Length && nums[i] + 1 == nums[i + 1])
+                    i++;
+
+                if (start != nums[i])
+                    result.Add($"{start}->{nums[i]}");
+                else
+                    result.Add($"{nums[i]}");
+            }
+        
+            return result;
+        }
+    }
+}
