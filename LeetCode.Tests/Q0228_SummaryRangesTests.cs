5b067fc259366911838dc224ee902c793168f92b
ungtsuhan <ungtsuhan@gmail.com>
2023-06-13 20:31:53 +0800

    Add Q0228 solution

---
+++ LeetCode.Tests/Q0228_SummaryRangesTests.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Tests
+{
+    public class Q0228Tests
+    {
+        [Theory]
+        [InlineData(new[] { 0, 1, 2, 4, 5, 7 }, new[] { "0->2", "4->5", "7" })]
+        [InlineData(new[] { 0, 2, 3, 4, 6, 8, 9 }, new[] { "0", "2->4", "6", "8->9" })]
+        public void Test(int[] nums, string[] expectedResult)
+        {
+            // Arrange
+
+            // Act
+            var actualResult = Q0228.SummaryRanges(nums);
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
