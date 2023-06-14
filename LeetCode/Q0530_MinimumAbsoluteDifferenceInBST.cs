8a03258d0f791892b45f030ae533b71777c68222
ungtsuhan <ungtsuhan@gmail.com>
2023-06-14 23:35:57 +0800

    Add Q0530 solution

---
+++ LeetCode/Q0530_MinimumAbsoluteDifferenceInBST.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.Common.Models;
+
+namespace LeetCode
+{
+    public class Q0530
+    {
+        int minDifference = int.MaxValue;
+        TreeNode? prevNode;
+
+        public int GetMinimumDifference(TreeNode root)
+        {
+            DFS(root);
+            return minDifference;
+        }
+
+        private void DFS(TreeNode? node)
+        {
+            if (node == null)
+                return;
+
+            DFS(node.left);
+
+            if (prevNode != null)
+                minDifference = Math.Min(minDifference, node.val - prevNode.val);
+
+            prevNode = node;
+            DFS(node.right);
+        }
+    }
+}
