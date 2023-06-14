4445cad223e08dc87d8750b9ca2d3dcf61c260a5
ungtsuhan <ungtsuhan@gmail.com>
2023-06-14 23:35:41 +0800

    Add TreeNode model

---
+++ LeetCode.Common/Models/TreeNode.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Common.Models
+{
+    /// <summary>
+    /// Definition for a binary tree node
+    /// </summary>
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode? left;
+        public TreeNode? right;
+
+        public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
