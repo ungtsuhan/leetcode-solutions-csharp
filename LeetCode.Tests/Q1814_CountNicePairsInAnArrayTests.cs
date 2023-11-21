3afcf8f31982330c5691e1042b63c91f83a24352
ungtsuhan <ungtsuhan@gmail.com>
2023-11-21 20:59:11 +0800

    Add Q1814 solution

---
+++ LeetCode.Tests/Q1814_CountNicePairsInAnArrayTests.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Tests;
+public class Q1814Tests
+{
+    [Theory]
+    [InlineData(new int[] { 1, 1, 10, 10 }, 2)]
+    [InlineData(new int[] { 42, 11, 1, 97 }, 2)]
+    [InlineData(new int[] { 13, 10, 35, 24, 76 }, 4)]
+    [InlineData(new int[] { 432835222, 112141211, 5408045, 456281503, 283322436, 414281561, 37773, 286505682 }, 6)]
+    public void CountNicePairs_ValidInput_ReturnsCorrectResult(int[] nums, int expectedResult)
+    {
+        // Arrange
+
+        // Act
+        var actualResult = Q1814.CountNicePairs(nums);
+
+        // Assert
+        Assert.Equal(expectedResult, actualResult);
+    }
+}
---
+++ LeetCode/Q1814_CountNicePairsInAnArray.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode;
+
+public class Q1814
+{
+    private const int MODULUS = 1_000_000_007;
+
+    public static int CountNicePairs(int[] nums)
+    {
+        var numOfPairs = 0;
+        Dictionary<int, int> dict = [];
+        foreach (var num in nums)
+        {
+            int numMinusReverseNum = num - Reverse(num);
+            if (dict.TryGetValue(numMinusReverseNum, out int currentCount))
+            {
+                numOfPairs = (numOfPairs + currentCount) % MODULUS;
+                dict[numMinusReverseNum]++;
+            }
+            else
+            {
+                dict[numMinusReverseNum] = 1;
+            }
+        }
+        return numOfPairs;
+    }
+
+    private static int Reverse(int num)
+    {
+        int result = 0;
+        while (num > 0)
+        {
+            result = result * 10 + num % 10;
+            num /= 10;
+        }
+        return result;
+    }
+}
