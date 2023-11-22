8b31de8655728858804d9e0d7e59f1b0d3d00030
ungtsuhan <ungtsuhan@gmail.com>
2023-11-22 21:41:58 +0800

    Add Q1424 solution

---
+++ LeetCode.Tests/Q1424_DiagonalTraverseIITests.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode.Tests;
+
+public class Q1424Tests
+{
+    public static IEnumerable<object[]> TestData()
+    {
+        yield return new object[]
+        {
+            new List<IList<int>> { new List<int> { 1, 2, 3 }, new List<int> { 4, 5, 6 }, new List<int> { 7, 8, 9 } },
+            new List<int> { 1, 4, 2, 7, 5, 3, 8, 6, 9 }
+        };
+
+        yield return new object[]
+        {
+            new List<IList<int>> { new List<int> { 1, 2, 3 }, new List<int> { 4, 5, 6 }, new List<int> { 7, 8, 9 } },
+            new List<int> { 1, 4, 2, 7, 5, 3, 8, 6, 9 }
+        };
+
+        yield return new object[]
+        {
+            new List<IList<int>> { new List<int> { 1, 2, 3, 4, 5 }, new List<int> { 6, 7 }, new List<int> { 8 }, new List<int> { 9, 10, 11 }, new List<int> { 12, 13, 14, 15, 16 } },
+            new List<int> { 1, 6, 2, 8, 7, 3, 9, 4, 12, 10, 5, 13, 11, 14, 15, 16 }
+        };
+    }
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void FindDiagonalOrder_ValidInput_ReturnsCorrectResult(IList<IList<int>> nums, IList<int> expectedResult)
+    {
+        // Arrange
+
+        // Act
+        var actualResult = Q1424.FindDiagonalOrder(nums);
+
+        // Assert
+        Assert.Equal(expectedResult, actualResult);
+    }
+}
---
+++ LeetCode/Q1424_DiagonalTraverseII.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode;
+
+public class Q1424
+{
+    // Time Complexity: O(n log n)
+    // Space Complexity: O(n)
+
+    public static int[] FindDiagonalOrder(IList<IList<int>> nums)
+    {
+        List<(int sum, int row, int value)> list = [];
+        for (var row = 0; row < nums.Count; row++) // O(m * n)
+        {
+            for (var column = 0; column < nums[row].Count; column++)
+            {
+                list.Add((row + column, row, nums[row][column]));
+            }
+        }
+
+        int[] result = list
+            .OrderBy(x => x.sum) // O(n log n)
+            .ThenByDescending(x => x.row)
+            .Select(x => x.value)
+            .ToArray();
+
+        return result;
+    }
+}
