936c3f502424f1f50c18d3c4a934aa1de73bd335
ungtsuhan <ungtsuhan@gmail.com>
2023-11-24 23:09:59 +0800

    Add Q1561 solution

---
+++ LeetCode.Tests/Q1561_MaximumNumberOfCoinsYouCanGetTests.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Tests;
+
+public class Q1561Tests
+{
+    [Theory]
+    [InlineData(new int[] { 2, 4, 1, 2, 7, 8 }, 9)]
+    [InlineData(new int[] { 2, 4, 5 }, 4)]
+    [InlineData(new int[] { 9, 8, 7, 6, 5, 1, 2, 3, 4 }, 18)]
+    public void MaxCoins_ValidInput_ReturnsCorrectResult(int[] piles, int expectedResult)
+    {
+        // Arrange
+
+        // Act
+        var actualResult = Q1561.MaxCoins(piles);
+
+        // Assert
+        Assert.Equal(expectedResult, actualResult);
+    }
+}
---
+++ LeetCode/Q1561_MaximumNumberOfCoinsYouCanGet.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode;
+
+public class Q1561
+{
+    // Time Complexity: O(n log n)
+    // Space Complexity: O(1)
+
+    public static int MaxCoins(int[] piles)
+    {
+        Array.Sort(piles);
+        var chances = piles.Length / 3;
+        var maxCoins = 0;
+        for (var i = 0; i < chances; i++)
+        {
+            maxCoins += piles[piles.Length - 2 - (i * 2)];
+        }
+        return maxCoins;
+    }
+}
