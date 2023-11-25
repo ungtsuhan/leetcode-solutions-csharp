206ec63b00d2b559ba6f2688ac5ea059098bf50c
ungtsuhan <ungtsuhan@gmail.com>
2023-11-25 12:53:20 +0800

    Add Q1685 solution

---
+++ LeetCode.Tests/Q1685_SumOfAbsoluteDifferencesInASortedArrayTests.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Tests;
+
+public class Q1685Tests
+{
+    [Theory]
+    [InlineData(new int[] { 2, 3, 5 }, new int[] { 4, 3, 5 })]
+    [InlineData(new int[] { 1, 4, 6, 8, 10 }, new int[] { 24, 15, 13, 15, 21 })]
+    public void GetSumAbsoluteDifferences_ValidInput_ReturnsCorrectResult(int[] nums, int[] expectedResult)
+    {
+        // Arrange
+
+        // Act
+        var actualResult = Q1685.GetSumAbsoluteDifferences(nums);
+
+        // Assert
+        Assert.Equal(expectedResult, actualResult);
+    }
+}
---
+++ LeetCode/Q1685_SumOfAbsoluteDifferencesInASortedArray.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode;
+
+public class Q1685
+{
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
+    public static int[] GetSumAbsoluteDifferences(int[] nums)
+    {
+        int n = nums.Length;
+
+        int[] prefixSum = new int[n];
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            prefixSum[i] += nums[i];
+            if (i > 0) prefixSum[i] += prefixSum[i - 1];
+        }
+
+        int leftSum = 0;
+        int rightSum = prefixSum[n - 1];
+        for (int i = 0; i < n; i++)
+        {
+            if (i > 0) leftSum += nums[i - 1];
+            rightSum -= nums[i];
+            int leftCount = i;
+            int rightCount = n - i - 1;
+            int leftTotal = leftCount * nums[i] - leftSum;
+            int rightTotal = rightSum - rightCount * nums[i];
+            result[i] = leftTotal + rightTotal;
+        }
+        return result;
+    }
+}
